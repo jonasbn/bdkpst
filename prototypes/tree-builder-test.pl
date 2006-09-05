1d78017c3f6b08c4b56f59bf341d6135bbcdc66e
jonasbn <jonasbn@logiclab.dk>
2006-09-05 18:19:18 +0000

    Added DEBUG flag and CVS Id keyword
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/BDKPST/trunk@5490 e6edf6fb-f266-4316-afb4-e53d95876a76

--- prototypes/tree-builder-test.pl
+++ prototypes/tree-builder-test.pl
@@ -1,8 +1,12 @@
 #!/usr/bin/perl -w
 
+# $Id: tree-builder-test.pl,v 1.2 2006-09-05 18:19:18 jonasbn Exp $
+
 use strict;
 use Data::Dumper;
 use Tree::Simple;
+
+use constant DEBUG => 0;
 
 my $tree = Tree::Simple->new();
 
@@ -13,14 +17,14 @@ my $oldtree = $tree;
 my $j = 0;
 foreach my $number (@data) {
 	$j++;
-	print STDERR "\nWe have number: $number [$j]\n";
+	print STDERR "\nWe have number: $number [$j]\n" if DEBUG;
 	my @digits = split(//, $number, 4);
 	
 	for(my $i = 0; $i < scalar(@digits); $i++) {
 
-		print STDERR "We have digit: ".$digits[$i]."\n";
+		print STDERR "We have digit: ".$digits[$i]."\n" if DEBUG;
 		if ($i == 0) {
-			print STDERR "We are resetting to oldtree: $i\n";
+			print STDERR "We are resetting to oldtree: $i\n" if DEBUG;
 			$tree = $oldtree;
 		}
 		
@@ -29,7 +33,7 @@ foreach my $number (@data) {
  		my @children = $tree->getAllChildren();
  		my $child = undef;
  		foreach my $c (@children) {
- 			print STDERR "\$c: ".$c->getNodeValue()."\n";
+ 			print STDERR "\$c: ".$c->getNodeValue()."\n" if DEBUG;
  			if ($c->getNodeValue() == $subtree->getNodeValue()) {
 				$child = $c;
 				last;
@@ -37,10 +41,10 @@ foreach my $number (@data) {
  		}
 
   		if ($child) {
- 			print STDERR "We are represented at $i with $digits[$i], we go to next\n";
+ 			print STDERR "We are represented at $i with $digits[$i], we go to next\n" if DEBUG;
 		 	$tree = $child;
  		} else {
-			print STDERR "We are adding child ".$subtree->getNodeValue."\n";
+			print STDERR "We are adding child ".$subtree->getNodeValue."\n" if DEBUG;
 			$tree->addChild($subtree);
 		 	$tree = $subtree;
  		}
